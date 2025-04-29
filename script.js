e00799714b63354c6882da3ab446bd67296b8453
Alexalom99 <alexalom1999@gmail.com>
2025-04-29 20:35:31 +0200

    done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // ¡El país te necesita!
+
+const fs = require('fs');
+const path = require('path');
+
+subestaciones = fs.readdirSync(path.join('subestaciones'));
+
+for (file of subestaciones) {
+    let data = fs.readFileSync(path.join('subestaciones', file),'utf-8');
+    data = data.trim();
+    const [ first, second ] = data.split(',');
+    const result = first - second;
+    if (result == 15000) {
+        console.log(`Subestación con fallo detectada: ${ file }\n Lecturas: ${first},${second}\n Diferencia: ${result}`);
+        break;
+    }
+}
+
